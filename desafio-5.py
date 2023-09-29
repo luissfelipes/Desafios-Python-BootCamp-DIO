7d0ae2f4dc2d1f9896859d0cc31d96b7e615b94b
luissfelipes <luisfelipeww@gmail.com>
2023-09-29 10:49:06 -0300

    desafio pronto

---
+++ desafio-5.py
@@ -0,0 +1,9 @@
+valor = float(input())
+
+
+if valor > 0:
+    print(f'Deposito realizado com sucesso! \nSaldo atual: R$ {valor:.2f}')
+elif valor < 0:
+    print('Valor invalido! Digite um valor maior que zero.')
+else:
+    print('Encerrando programa...')
